39d567c273abb8df91e5acdb3d1b1ec709f75dfb
agorzki_centrum <gorzki@gmx.com>
2021-09-02 19:12:20 +0200

    first commit

---
+++ src/main/java/pl/gorzki/fmspring/FMSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.gorzki.fmspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FMSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FMSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gorzki/fmspring/FMSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gorzki.fmspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FMSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
