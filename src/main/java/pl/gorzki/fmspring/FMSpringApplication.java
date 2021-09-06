63d5cebe9f829cef0b941c858ffac58062bed7dd
ER4 <gorzki@gmx.com>
2021-09-06 14:25:18 +0200

    first service

--- src/main/java/pl/gorzki/fmspring/FMSpringApplication.java
+++ src/main/java/pl/gorzki/fmspring/FMSpringApplication.java
@@ -1,12 +1,30 @@
 package pl.gorzki.fmspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class FMSpringApplication {
+public class FMSpringApplication implements CommandLineRunner {
+
+	public FMSpringApplication(FaultService service) {
+		this.service = service;
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(FMSpringApplication.class, args);
-	}
+        SpringApplication.run(FMSpringApplication.class, args);
+    }
+
+
+	private  final  FaultService service;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+//        FaultService service = new FaultService();
+        List<Fault> faults = service.findAll();
+        faults.forEach(System.out::println);
+    }
 }
--- src/main/java/pl/gorzki/fmspring/Fault.java
+++ src/main/java/pl/gorzki/fmspring/Fault.java
@@ -1,11 +1,42 @@
 package pl.gorzki.fmspring;
 
+import java.util.StringJoiner;
+
 public class Fault {
-Long id;
-String faultDescribe;
-FaultStatus status;
-TechArea area;
-Specialist specialist;
-Assigner whoAssigned;
-Notifier whoNotify;
+    Long id;
+    String faultDescribe;
+    FaultStatus status;
+    TechArea area;
+    Specialist specialist;
+    Assigner whoAssigned;
+    Notifier whoNotify;
+
+    public Fault(Long id, String faultDescribe, FaultStatus status, TechArea area, Specialist specialist, Assigner whoAssigned, Notifier whoNotify) {
+        this.id = id;
+        this.faultDescribe = faultDescribe;
+        this.status = status;
+        this.area = area;
+        this.specialist = specialist;
+        this.whoAssigned = whoAssigned;
+        this.whoNotify = whoNotify;
+    }
+
+    public Fault(Long id, String faultDescribe) {
+        this.id = id;
+        this.faultDescribe = faultDescribe;
+
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Fault.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("faultDescribe='" + faultDescribe + "'")
+                .add("status=" + status)
+                .add("area=" + area)
+                .add("specialist=" + specialist)
+                .add("whoAssigned=" + whoAssigned)
+                .add("whoNotify=" + whoNotify)
+                .toString();
+    }
 }
