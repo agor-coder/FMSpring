60166abef612b61a068d5c741df6412417e30e17
Andrzej Gorzkiewicz <gorzki@gmx.com>
2021-10-05 19:27:25 +0200

    @EnableJpaAuditing @EntityListeners(AuditingEntityListener.class)

--- src/main/java/pl/gorzki/fmspring/FMSpringApplication.java
+++ src/main/java/pl/gorzki/fmspring/FMSpringApplication.java
@@ -2,8 +2,10 @@ package pl.gorzki.fmspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FMSpringApplication {
 
 
