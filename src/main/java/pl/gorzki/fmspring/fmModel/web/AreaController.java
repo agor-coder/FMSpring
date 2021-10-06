63d90f552e06f6fb77940f0cfd79d5728314eec0
ER4 <gorzki@gmx.com>
2021-10-06 14:32:20 +0200

    REST - Area findById and addArea

--- src/main/java/pl/gorzki/fmspring/fmModel/web/AreaController.java
+++ src/main/java/pl/gorzki/fmspring/fmModel/web/AreaController.java
@@ -2,11 +2,16 @@ package pl.gorzki.fmspring.fmModel.web;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.gorzki.fmspring.fmModel.application.port.AreaUseCase;
+import pl.gorzki.fmspring.fmModel.application.port.AreaUseCase.CreateAreaCommand;
 import pl.gorzki.fmspring.fmModel.domain.TechArea;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,8 +32,27 @@ public class AreaController {
         return service.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> geById(@PathVariable Long id) {
+        return service
+                .fidById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TechArea addArea(@Valid @RequestBody RestAreaCommand command) {
+        return service.addArea(command.toCreateCommand());
+    }
 
+    @Data
+    private static class RestAreaCommand {
+        @NotBlank(message = "podaj nazwe")
+        private String areaName;
 
-
+        private CreateAreaCommand toCreateCommand() {
+            return new CreateAreaCommand(areaName);
+        }
+    }
 }
