e292b71065fbb3931ee9552fee3440e06b4be445
Andrzej Gorzkiewicz <gorzki@gmx.com>
2021-10-15 22:21:05 +0200

    addNotifier in usersController

--- src/main/java/pl/gorzki/fmspring/users/web/UsersController.java
+++ src/main/java/pl/gorzki/fmspring/users/web/UsersController.java
@@ -10,7 +10,6 @@ import pl.gorzki.fmspring.users.application.port.UserUseCase.CreateUserCommand;
 import pl.gorzki.fmspring.users.domain.UserEntity;
 
 
-
 @RestController
 @AllArgsConstructor
 @RequestMapping("/users")
@@ -26,6 +25,12 @@ public class UsersController {
 
     }
 
+    @PostMapping("/notifier")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserEntity registerNotifier(@RequestBody RestNotifierCommand command) {
+        return service.register(command.toCreateNotifierCommand());
+    }
+
 
     @Data
     private static class RestUserCommand {
@@ -38,6 +43,19 @@ public class UsersController {
 
         CreateUserCommand toCreateCommand() {
             return new CreateUserCommand(password, firstName, lastName, phone, emailUserName, role);
+        }
+    }
+
+    @Data
+    private static class RestNotifierCommand {
+        private String password;
+        private String firstName;
+        private String lastName;
+        private String phone;
+        private String emailUserName;
+
+        CreateUserCommand toCreateNotifierCommand() {
+            return new CreateUserCommand(password, firstName, lastName, phone, emailUserName, "ROLE_NOTIFIER");
         }
     }
 }
