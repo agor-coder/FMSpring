d7776620bbd56f0305d8d66cb87b517dab947551
Andrzej Gorzkiewicz <gorzki@gmx.com>
2021-10-17 23:28:16 +0200

    countOfSpecialist in FaultJpaRepository

--- src/main/java/pl/gorzki/fmspring/fault/db/FaultJpaRepository.java
+++ src/main/java/pl/gorzki/fmspring/fault/db/FaultJpaRepository.java
@@ -1,8 +1,20 @@
 package pl.gorzki.fmspring.fault.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.gorzki.fmspring.fault.domain.Fault;
+import pl.gorzki.fmspring.fault.domain.FaultStatus;
+import pl.gorzki.fmspring.users.domain.UserEntity;
 
 public interface FaultJpaRepository extends JpaRepository<Fault, Long> {
 
+
+    @Query(
+            "  SELECT COUNT (i) FROM Fault i "
+                    + " WHERE " +
+                    " i.specialist = :specialist " +
+                    " AND i.status=:status "
+    )
+    int countOfSpecialist(@Param("specialist") UserEntity specialist, @Param("status") FaultStatus status);
 }
