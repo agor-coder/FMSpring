345d6123cbc8dbd11e6d2b0230738d69df437e3b
Andrzej Gorzkiewicz <gorzki@gmx.com>
2021-11-16 00:10:30 +0100

    ManipulateFaultServiceTest added

---
+++ src/test/java/pl/gorzki/fmspring/fault/application/ManipulateFaultServiceTest.java
@@ -0,0 +1,28 @@
+package pl.gorzki.fmspring.fault.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManipulateFaultServiceTest {
+
+    @Test
+    void addFault() {
+    }
+
+    @Test
+    void removeFaultById() {
+    }
+
+    @Test
+    void updateFault() {
+    }
+
+    @Test
+    void assignFault() {
+    }
+
+    @Test
+    void changeStatus() {
+    }
+}
