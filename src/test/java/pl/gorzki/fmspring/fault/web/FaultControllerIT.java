ec3114f589817f1f6f46fcf2ec75ea357317a66b
Andrzej Gorzkiewicz <gorzki@gmx.com>
2021-11-15 23:43:05 +0100

    FaultControllerIT

--- src/test/java/pl/gorzki/fmspring/fault/web/FaultControllerIT.java
+++ src/test/java/pl/gorzki/fmspring/fault/web/FaultControllerIT.java
@@ -1,5 +1,6 @@
-package pl.gorzki.fmspring.fault.application;
+package pl.gorzki.fmspring.fault.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,14 +12,13 @@ import pl.gorzki.fmspring.area.domain.TechArea;
 import pl.gorzki.fmspring.fault.application.port.ManipulateFaultUseCase;
 import pl.gorzki.fmspring.fault.application.port.ManipulateFaultUseCase.CreateFaultCommand;
 import pl.gorzki.fmspring.fault.application.port.ManipulateFaultUseCase.UpdateFaultCommand;
-import pl.gorzki.fmspring.fault.application.port.QueryFaultUseCase;
 import pl.gorzki.fmspring.fault.domain.Fault;
 import pl.gorzki.fmspring.users.application.port.UserUseCase;
 import pl.gorzki.fmspring.users.application.port.UserUseCase.CreateUserCommand;
-import pl.gorzki.fmspring.users.db.UserJpaRepository;
 import pl.gorzki.fmspring.users.domain.UserEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static pl.gorzki.fmspring.fault.domain.FaultStatus.ASSIGNED;
@@ -26,23 +26,21 @@ import static pl.gorzki.fmspring.fault.domain.FaultStatus.ASSIGNED;
 
 @SpringBootTest
 @AutoConfigureTestDatabase// with h2
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class FaultServiceTest {
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+class FaultControllerIT {
     @Autowired
     ManipulateFaultUseCase manipulateFaultService;
-    @Autowired
-    QueryFaultUseCase queryFaultService;
-    @Autowired
-    UserJpaRepository userJpaRepository;
     @Autowired
     AreaUseCase areaService;
     @Autowired
     UserUseCase userService;
+    @Autowired
+    FaultController controller;
 
 
-    @Test
-    public void findAllFaultsByUser() {
-//        given
+
+    @BeforeEach
+    private void initData() {
         TechArea area1 = areaService.addArea(new CreateAreaCommand("maszynownia"));
         TechArea area2 = areaService.addArea(new CreateAreaCommand("elektr"));
         TechArea area3 = areaService.addArea(new CreateAreaCommand("kotlownia"));
@@ -62,19 +60,27 @@ class FaultServiceTest {
         ));
 
 
-        Fault fault1 = manipulateFaultService.addFault(new CreateFaultCommand("zwarcie", area1.getId(), notifier1.getId()));
+        manipulateFaultService.addFault(new CreateFaultCommand("zwarcie", area1.getId(), notifier1.getId()));
         manipulateFaultService.addFault(new CreateFaultCommand("brak", area2.getId(), notifier2.getId()));
         manipulateFaultService.addFault(new CreateFaultCommand("nie ma", area1.getId(), notifier1.getId()));
         manipulateFaultService.addFault(new CreateFaultCommand("spalony", area2.getId(), notifier2.getId()));
         manipulateFaultService.addFault(new CreateFaultCommand("NOWA", area3.getId(), notifier2.getId()));
 
         manipulateFaultService.updateFault(new UpdateFaultCommand(
-                fault1.getId(), null, ASSIGNED, null, spec.getId(), assigner.getId(), null));
+                5L, null, ASSIGNED, null, spec.getId(), assigner.getId(), null));
+    }
+
+
+
+    @Test
+    public void findAllFaultsByUser() {
+//        given
+
 
 //        when
-        List<Fault> byNotifier = queryFaultService.findAllByUser(notifier2);
-        List<Fault> bySpec = queryFaultService.findAllByUser(spec);
-        List<Fault> byAssigner = queryFaultService.findAllByUser(assigner);
+        List<Fault> byNotifier = controller.getAllByUser(2L);
+        List<Fault> bySpec = controller.getAllByUser(3L);
+        List<Fault> byAssigner = controller.getAllByUser(4L);
 //        then
         assertEquals(3, byNotifier.size());
         assertEquals(1, bySpec.size());
@@ -84,37 +90,10 @@ class FaultServiceTest {
     @Test
     public void findAllFaults() {
 //        given
-        TechArea area1 = areaService.addArea(new CreateAreaCommand("maszynownia"));
-        TechArea area2 = areaService.addArea(new CreateAreaCommand("elektr"));
-        TechArea area3 = areaService.addArea(new CreateAreaCommand("kotlownia"));
 
-        UserEntity notifier1 = userService.register(new CreateUserCommand(
-                "123", "Peter", "Novak", "12345", "peter@2.pl", "ROLE_NOTIFIER"
-        ));
-        UserEntity notifier2 = userService.register(new CreateUserCommand(
-                "123", "Peter", "Smith", "12345", "peter2@2.pl", "ROLE_NOTIFIER"
-        ));
-
-        UserEntity spec = userService.register(new CreateUserCommand(
-                "123", "John", "Spec", "12345", "spec@2.pl", "ROLE_SPECIALIST"
-        ));
-        UserEntity assigner = userService.register(new CreateUserCommand(
-                "123", "Mike", "Assign", "12345", "assigner@2.pl", "ROLE_ASSIGNER"
-        ));
-
-
-        Fault fault1 = manipulateFaultService.addFault(new CreateFaultCommand("zwarcie", area1.getId(), notifier1.getId()));
-        manipulateFaultService.addFault(new CreateFaultCommand("brak", area2.getId(), notifier2.getId()));
-        manipulateFaultService.addFault(new CreateFaultCommand("nie ma", area1.getId(), notifier1.getId()));
-        manipulateFaultService.addFault(new CreateFaultCommand("spalony", area2.getId(), notifier2.getId()));
-        manipulateFaultService.addFault(new CreateFaultCommand("NOWA", area3.getId(), notifier2.getId()));
-
-        manipulateFaultService.updateFault(new UpdateFaultCommand(
-                fault1.getId(), null, ASSIGNED, null, spec.getId(), assigner.getId(), null));
 //        when
-        List<Fault> list1 = queryFaultService.findAll();
+        List<Fault> list1 = controller.getAll(Optional.empty(), Optional.empty());
 //        then
-        System.out.println(list1);
         assertEquals(5, list1.size());
     }
 }
---
+++ src/test/java/pl/gorzki/fmspring/fault/web/FaultControllerIT_OnActiveDb.java
@@ -0,0 +1,33 @@
+package pl.gorzki.fmspring.fault.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pl.gorzki.fmspring.fault.domain.Fault;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+//////////////na aktywnej bazie
+@SpringBootTest
+class FaultControllerIT_OnActiveDb {
+
+    @Autowired
+    FaultController controller;
+
+
+  @Test
+    public void findAllByUser() {
+//        given
+
+//        when
+        List<Fault> list1 = controller.getAllByUser(1L);
+//        then
+        System.out.println(list1);
+        assertEquals(3,list1.size());
+    }
+
+}
